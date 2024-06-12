ab72e65daf16e96b2d49d91e34af4fe9096ea211
DKrawczyk <d.krawczyk.nam@gmail.com>
2024-06-13 01:16:36 +0200

    Initial functionalities

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,22 +1,35 @@
 import { Box } from "@mantine/core";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import "./styles.css";
 import { ChatHeader } from "./ChatHeader";
 import { ChatBody } from "./ChatBody";
 import { ChatIcon } from "./ChatIcon";
 
 export const MainPage: FC = () => {
+  const [isPopup, setIsPopup] = useState<boolean>(false);
   const [chatVisibility, setChatVisibility] = useState<boolean>(false);
+  useEffect(() => {
+    const isFirstTimeVisitor = localStorage.getItem("isFirstTimeUser") === null;
+
+    if (isFirstTimeVisitor) {
+      localStorage.setItem("isFirstTimeUser", JSON.stringify(true));
+
+      setTimeout(() => {
+        setIsPopup(true);
+        setChatVisibility(true);
+        localStorage.setItem("isFirstTimeUser", JSON.stringify(false));
+      }, 2000);
+    }
+  }, []);
+
   const handler = () => {
     setChatVisibility(!chatVisibility);
+    setIsPopup(false);
   };
-  const isPopup = true;
   return (
     <Box w={"100vw"} h={"100vh"} bg={"white"} pos={"relative"}>
       {chatVisibility && (
         <Box
-        //   direction={"column"}
-        //   justify={"space-between"}
           pos={"absolute"}
           right={20}
           bottom={100}
@@ -30,20 +43,6 @@ export const MainPage: FC = () => {
           <ChatHeader chatVisibilityHandler={handler} isPopup={isPopup} />
           <ChatBody isPopup={isPopup} />
         </Box>
-        // <Box
-        //   pos={"absolute"}
-        //   right={20}
-        //   bottom={100}
-        //   w={380}
-        //   h={322}
-        //   style={{
-        //     borderRadius: 9,
-        //     boxShadow: "0px 20px 50px #00000027",
-        //   }}
-        // >
-        //   <ChatHeader chatVisibilityHandler={handler} isPopup />
-        //   <ChatBody />
-        // </Box>
       )}
       <ChatIcon chatVisibilityHandler={handler} />
     </Box>
